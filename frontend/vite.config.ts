d89803f9bedbb8f13fd91561ce0e0f91d0846951
OnemanwithOneserver <onemanwithoneserver@gmail.com>
2026-08-18 18:05:02 +0530

    ,

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,4 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   base: '/YardStack/',
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    include: ['@emotion/react', '@emotion/styled']
+  }
 })
